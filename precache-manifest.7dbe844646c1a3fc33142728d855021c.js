4166c2a7cc8f3b2788162cbfb4dcdf6bf170f3f6
saurabh <saurabh@thirdrocktechkno.com>
2022-04-04 20:43:42 +0530

    Updates

---
+++ precache-manifest.7dbe844646c1a3fc33142728d855021c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4911700f56ec81122fc4d944231ac89",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "0784997ae13cecac1fc7",
+    "url": "/To-Do-List/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "264f22f65304ac6fc6e8",
+    "url": "/To-Do-List/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "0784997ae13cecac1fc7",
+    "url": "/To-Do-List/static/js/2.8c3b2665.chunk.js"
+  },
+  {
+    "revision": "264f22f65304ac6fc6e8",
+    "url": "/To-Do-List/static/js/main.10a5f786.chunk.js"
+  },
+  {
+    "revision": "dd7bf03bf03f9df51f30",
+    "url": "/To-Do-List/static/js/runtime~main.88207b6a.js"
+  }
+]);
